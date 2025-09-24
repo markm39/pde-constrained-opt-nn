e64e92480ddc3d46858e6e5d5a2afc8b7bf920c7
markm39 <markmiller0470@gmail.com>
2025-09-24 13:05:46 -0500

    Initial commit

---
+++ activate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Activation script for the PDE solver virtual environment
+
+echo "Activating virtual environment for PDE solver..."
+source venv/bin/activate
+echo "Virtual environment activated!"
+echo "To run the Jupyter notebook, use: jupyter notebook heat_equation_nn_solver.ipynb"
